fdd1aa16ff1360c1e1b052f954e43803745766b8
haptichash <harshitks2203@gmail.com>
2019-10-31 03:35:48 -0400

    updated chatListComp

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -3,60 +3,68 @@ import ChatListComp from '../chatlist/chatlist';
 const fb = require("firebase");
 
 class dashboardComp extends React.Component {
-    
-    state = {  }
-
     constructor() {
         super();
         this.state = {
-            selChat : null,
-            newChatFormVisi : false,
-            email : null,
-            chats : []
+          selectedChat: null,
+          newChatFormVisible: false,
+          email: null,
+          friends: [],
+          chats: []
         };
-    }
+      }
+    
+      render() {
+    
+        const { classes } = this.props;
+    
+        if(this.state.email) {
+          return(
+            <div className='dashboard-container' id='dashboard-container'>
+              
+              <ChatListComp history={this.props.history} 
+                userEmail={this.state.email} 
+                selectChatFn={this.selectChat} 
+                chats={this.state.chats} 
+                selectedChatIndex={this.state.selectedChat}
+                newChatBtnFn={this.newChatBtnClicked}>
+              </ChatListComp>
+   
+            </div>
+          );
+        } else {
+          return(<div>LOADING....</div>);
+        }
+      }
 
-    render() { 
-        return (
-        <div>
-            <ChatListComp 
-            history={this.props.history}
-            newChatBtnFunc={this.newChatButtonClicked}
-            selectChatFunc={this.selectaChat}
-            usrchats={this.state.chats}
-            usremails={this.state.email}
-            selChatIndex={this.state.selChat}> </ChatListComp>
-        </div>
-        );
-    }
 
-    selectaChat = (chatIndex) => {
-        console.log('A chat is selected.', chatIndex);
-    }
+        selectChat = async (chatIndex) => {
+            console.log('Selected chat is ', chatIndex);
+      }
 
-    newChatButtonClicked = () => this.setState({ newChatFormVisi: true, selChat: null })
+      newChatBtnClicked = () => this.setState({ newChatFormVisible: true, selectedChat: null });
 
-    componentDidMount = () => {
-        fb.auth().onAuthStateChanged(async _usr => {
-          if(!_usr)
-            this.props.history.push('/login');
-          else {
-            await fb
-              .firestore()
-              .collection('chats')
-              .where('users', 'array-contains', _usr.email)
-              .onSnapshot(async res => {
-                const chats = res.docs.map(_doc => _doc.data());
-                await this.setState({
-                  email: _usr.email,
-                  chats: chats,
-                  friends: []
-                });
-                console.log(this.state);
-              })
-          }
-      });
-    }
+
+      componentDidMount = () => {
+          fb.auth().onAuthStateChanged(async _usr => {
+            if(!_usr)
+              this.props.history.push('/login');
+            else {
+              await fb
+                .firestore()
+                .collection('chats')
+                .where('users', 'array-contains', _usr.email)
+                .onSnapshot(async res => {
+                  const chats = res.docs.map(_doc => _doc.data());
+                  await this.setState({
+                    email: _usr.email,
+                    chats: chats,
+                    friends: []
+                  });
+                })
+            }
+        });
+      }
 }
   
  
