eb5246879200be2114f539f376ec888c335706ae
haptichash <harshitks2203@gmail.com>
2019-11-09 00:50:14 -0500

    displaying the chat

--- src/components/chatview/chatview.js
+++ src/components/chatview/chatview.js
@@ -4,10 +4,39 @@ import { withStyles } from '@material-ui/core/styles';
 
 class ChatViewComponent extends React.Component {
 
+    componentDidUpdate = () => {
+        const container = document.getElementById('chatview-container');
+        if(container)
+            container.scrollTo(0, container.scrollHeight);
+    }
+
     render() {
         
-        const{ classes } = this.props;
+        const{ classes, chat, user } = this.props;
 
+        if(chat === undefined) {
+            return(<main id="chatview-container" className={classes.content}> </main>);
+        }
+        else {
+            return(
+                <div>
+                    <div className={classes.chatHeader}>
+                        Your chat with <b> {chat.users.filter(_usr => _usr !== user)} </b>
+                    </div>
+                    <main id="chatview-container" className={classes.content}>
+                        {
+                            chat.messages.map((_msg, _index) => {
+                                return(
+                                    <div key={_index} className={_msg.sender === user ? classes.userSent : classes.friendSent}>
+                                        { _msg.message}
+                                    </div>
+                                )
+                            })
+                        }
+                    </main>
+                </div>
+            )
+        }
         return( <div className={classes.content}>Hello from chat view component</div> );
 
     }
